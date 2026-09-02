b5291223996ec7a2566c1607eb9c991acbe673d1
kqtagiri <mezko321@gmail.com>
2026-09-02 18:31:25 +0300

    feat: add gin server and ping endpoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	fmt.Println("Smart Train API start")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "HELLO FROM GIN")
+	})
+
+	r.Run(":9111")
+	fmt.Println("Программа завершена")
 
 }
